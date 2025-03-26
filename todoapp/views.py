43a274337741f44c21f6aeb868ec97f4d7667f1d
admin <chsh055@ess.ais.ac.nz>
2025-03-26 21:25:54 +1300

    logout done

--- todoapp/views.py
+++ todoapp/views.py
@@ -7,6 +7,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from todoapp.serializers import UserSerializer
 
@@ -20,3 +21,11 @@ class LoginView(ObtainAuthToken):
         response = super(LoginView, self).post(request, *args, **kwargs)
         token = Token.objects.get(key=response.data['token'])
         return Response({'token': token.key, 'user_id': token.user_id})
+
+class LogoutView(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=200)
+
